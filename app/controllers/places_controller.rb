eedb2b0d2574578464265db2920d8674ac1c1642
swong11 <sunwong11@gmail.com>
2015-05-06 00:20:34 +0000

    Added functionality to the create button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,4 +6,16 @@ class PlacesController < ApplicationController
 	def new
 		@place = Place.new
 	end
+
+	def create
+		Place.create(place_params) # sends what the user inputted from form into the DB
+		redirect_to root_path  #redirect user back to home
+	end
+
+	private
+# suck in the values from the user after they press create
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
+	end
+
 end
