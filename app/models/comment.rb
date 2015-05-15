3bc38d8b7cf5f108813e481b360b67e982f0ea43
swong11 <sunwong11@gmail.com>
2015-05-15 01:29:34 +0000

    Changed the rating from textbox to dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+
+#An example of a dictionary or a hash table
+#The left is what the users will see. The right is what's stored in DB
+	RATINGS = {
+		'One Star' => '1_star', 
+		'Two Stars' => '2_stars',
+		'Three Stars' => '3_stars',
+		'Four Stars' => '4_stars',
+		'Five Stars' => '5_stars'
+ 	}
+
+ 	def humanized_rating
+ 		RATINGS.invert[self.rating]
+ 	end
 end
