7bf510fda453b3b598a63625de1de4f6db5afe7b
swong11 <sunwong11@gmail.com>
2015-05-18 23:57:16 +0000

    Trigger the emails when comments get added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+	after_create :send_comment_email
 
 #An example of a dictionary or a hash table
 #The left is what the users will see. The right is what's stored in DB
@@ -15,4 +16,9 @@ class Comment < ActiveRecord::Base
  	def humanized_rating
  		RATINGS.invert[self.rating]
  	end
+
+ 	def send_comment_email
+ 		NotificationMailer.comment_added(self).deliver
+ 	end
+
 end
