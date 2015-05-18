9a40e35445400f35bfefa8de4ca98e72543cc7f1
swong11 <sunwong11@gmail.com>
2015-05-18 09:01:53 +0000

    Preview Automated Email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,18 @@ Teaster::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
   config.action_mailer.default_url_options = { :host => 'localhost:3030'}
   config.cache_classes = false
 
@@ -16,7 +27,7 @@ Teaster::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
