26465bb6b4f5b46a5a10c41cab805e1c96cf6d6d
swong11 <sunwong11@gmail.com>
2015-06-03 19:24:40 +0000

    Remove line on navbar

--- test/factories.rb
+++ test/factories.rb
@@ -1,5 +1,22 @@
 FactoryGirl.define do
   factory :user do
-    email "hello123@gmail.com"
+    email "hello345345345345435@gmail.com"
+    password "abcdefghi345345345"
   end
+
+  factory :comment do
+    rating "1_star"
+    message "Hi"
+    user
+    place 
+
+  end
+
+  factory :place do
+  	name "Location"
+  	address "1182 drive"
+  	description "Hi"
+  	user 
+  end
+
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,12 +1,18 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  test "comments" do
-  	comment = Comment.create(:message => 'Hi', :rating => 'One Star', :user => 'sdfsdf@gmail.com')
-  	puts comment.inspect
+  test "comment_tester" do
+  	comment = FactoryGirl.create(:comment)
+  	# expected = 'One star'
+  	# actual = comment.comment_tester
+  	# assert_equal expected, actual
   end
 
 end
+
+
+# keep getting error email has already been taken. tried  db:test:prepare? Doesn't work.
+# Database Cleaner where to put gemfile?
 
 
 
